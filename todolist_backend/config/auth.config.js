546e04561c5b50813515e9b3553452ba7d34663e
khy724 <81826907+khy724@users.noreply.github.com>
2022-07-13 00:01:58 +0530

    Delete todolist_backend/config directory

--- todolist_backend/config/auth.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    secret: "bezkoder-secret-key",
-    user: "abxycz325@gmail.com", 
-    pass: "rwbtulsmbnbbhruq", 
-    jwtExpiration: 3600,           // 1 hour
-    jwtRefreshExpiration: 86400,   // 24 hours
-  };
